68e0191fd67ebd5d23e9af96fbf0484e3cfa9ea1
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2024-10-02 13:20:32 -0400

    Creates an individual test for LoopCharacterizer

--- test/testLoopCharacterizer.ts
+++ test/testLoopCharacterizer.ts
@@ -1,6 +1,6 @@
 import { FunctionJp, Loop } from "@specs-feup/clava/api/Joinpoints.js"
 import Query from "@specs-feup/lara/api/weaver/Query.js";
-import { LoopCharacterizer } from "./LoopCharacterizer.js";
+import { LoopCharacterizer } from "../src/LoopCharacterizer.js";
 
 for (const fun of Query.search(FunctionJp, { name: "loop_characterization" })) {
     for (const loop of Query.searchFrom(fun, Loop)) {
