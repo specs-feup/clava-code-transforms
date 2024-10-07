948559c2e6c2b26ee130dcb6c5fabfd6f2846213
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2024-10-07 11:52:34 -0400

    Converts Outliner to TS

--- test/TestOutliner.ts
+++ test/TestOutliner.ts
@@ -1,50 +1,56 @@
-laraImport("weaver.WeaverJps");
-laraImport("weaver.Query");
-laraImport("clava.ClavaJoinPoints");
-laraImport("clava.code.Outliner");
-//laraImport("AstDumper");
-
+import { Pragma, Scope, Statement, WrapperStmt } from "@specs-feup/clava/api/Joinpoints.js";
+import Query from "@specs-feup/lara/api/weaver/Query.js";
+import Outliner from "../src/Outliner.js";
+import { AstDumper } from "./AstDumper.js";
 
 function main() {
+    const dumper = new AstDumper();
+    const astDump = dumper.dump();
+    console.log(astDump);
+
     // We want the wrapper statement around the pragma, not the pragma itself
     // as the wrapper statements both share the same parent (i.e., are on the same scope)
-    //const dumper = new AstDumper();
-    //dumper.dump();
+    for (const beginPragma of Query.search(Pragma, { "content": "begin_outline" })) {
+        const beginWrapper = beginPragma.parent as WrapperStmt;
+        let endWrapper = beginWrapper as WrapperStmt;
+        const scope = beginWrapper.parent as Scope;
 
-    for (const pragma of Query.search("pragma", { "content": "begin_outline" })) {
-        var beginPragma = pragma.parent;
-        var endPragma = null;
+        for (const stmt of scope.children) {
+            let foundBegin = false;
+            if (stmt === beginWrapper) {
+                foundBegin = true;
+                continue;
+            }
 
-        var next = beginPragma.rightJp;
-        while (next != null) {
-            if (next.joinPointType === "wrapperStmt" && next.numChildren > 0) {
-                if (next.children[0].content === "end_outline") {
-                    endPragma = next;
+            if (stmt instanceof WrapperStmt) {
+                const pragma = stmt.children[0];
+                if (pragma instanceof Pragma && pragma.content === "end_outline" && foundBegin) {
+                    endWrapper = stmt;
                     break;
                 }
             }
-            next = next.rightJp;
         }
-        if (beginPragma == null || endPragma == null) {
-            println("Could not find the region for outlining! Begin = " + beginPragma + ", end = " + endPragma);
+
+        if (beginWrapper == null || endWrapper == null) {
+            console.log("Could not find the region for outlining! Begin = " + beginWrapper + ", end = " + endWrapper);
             continue;
         }
         else {
-            processOutliningRegion(beginPragma, endPragma);
+            processOutliningRegion(beginWrapper, endWrapper);
         }
     }
 }
 
-function processOutliningRegion(beginPragma, endPragma) {
-    const begin = beginPragma.siblingsRight[0];
-    const end = endPragma.siblingsLeft[endPragma.siblingsLeft.length - 1];
+function processOutliningRegion(beginPragma: WrapperStmt, endPragma: WrapperStmt) {
+    const begin = beginPragma.siblingsRight[0] as Statement; // Stmt immediately before the first pragma
+    const end = endPragma.siblingsLeft[endPragma.siblingsLeft.length - 1] as Statement; // Stmt immediately after the last pragma
     beginPragma.detach();
     endPragma.detach();
 
-    println("Beginning the outline process...");
+    console.log("Beginning the outline process...");
     const outliner = new Outliner();
     outliner.outline(begin, end);
-    println("Outlining finished!");
+    console.log("Outlining finished!");
 }
 
 main();
