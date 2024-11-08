6855c78c5f2b48ddf20d1491cb3aa70b01b8d5cc
Tiago Lascasas dos Santos <tiagolascasas@outlook.com>
2024-11-08 16:37:28 -0500

    Adds rebuild() to the flattening + propagation test

--- test/TestFlatteningConstant.ts
+++ test/TestFlatteningConstant.ts
@@ -3,12 +3,15 @@ import { ArrayFlattener } from "../src/ArrayFlattener.js";
 import { FoldingPropagationCombiner } from "../src/constfolding/FoldingPropagationCombiner.js";
 import { AstDumper } from "./AstDumper.js";
 import { FunctionJp } from "@specs-feup/clava/api/Joinpoints.js";
+import Clava from "@specs-feup/clava/api/clava/Clava.js";
 
 const dumper = new AstDumper();
 console.log(dumper.dump());
 
 const arrayFlattener = new ArrayFlattener();
 arrayFlattener.flattenAll();
+
+Clava.rebuild();
 
 const folder = new FoldingPropagationCombiner();
 for (const fun of Query.search(FunctionJp)) {
