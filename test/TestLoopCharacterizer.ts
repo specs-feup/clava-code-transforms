5bcda9db79b37b8be8525f567403bbbe81e719b0
tiagolascasas <tiagolascasas@outlook.com>
2025-03-12 23:53:30 +0000

    Adds AST visualization to a test

--- test/TestLoopCharacterizer.ts
+++ test/TestLoopCharacterizer.ts
@@ -1,6 +1,9 @@
 import { FunctionJp, Loop } from "@specs-feup/clava/api/Joinpoints.js"
 import Query from "@specs-feup/lara/api/weaver/Query.js";
 import { LoopCharacterizer } from "../src/LoopCharacterizer.js";
+import VisualizationTool from "@specs-feup/clava-visualization/api/VisualizationTool.js";
+
+await VisualizationTool.visualize();
 
 for (const fun of Query.search(FunctionJp, { name: "loop_characterization" })) {
     for (const loop of Query.searchFrom(fun, Loop)) {
