58e5843a7b22a9d69035cc301c85cf6fbb1fd5b9
tiagolascasas <tiagolascasas@outlook.com>
2025-04-23 23:57:36 +0100

    Updates publishing script

--- publish.js
+++ publish.js
@@ -3,6 +3,8 @@ import { readFileSync, writeFileSync } from 'fs';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
+const tag = process.argv[2];
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const packageJsonPath = resolve(__dirname, 'package.json');
 const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf-8'));
@@ -17,14 +19,14 @@ const hours = String(now.getHours()).padStart(2, '0');
 const minutes = String(now.getMinutes()).padStart(2, '0');
 const seconds = String(now.getSeconds()).padStart(2, '0');
 const timestamp = `${year}${month}${day}${hours}${minutes}.${seconds}`;
-const stagingVersion = `${originalVersion}-staging.${timestamp}`;
+const stagingVersion = `${originalVersion}-${tag}.${timestamp}`;
 
 packageJson.version = stagingVersion;
 writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 console.log(`Updated version to ${stagingVersion}`);
 
 execSync('npm run build', { stdio: 'inherit' });
-execSync('npm publish --tag staging', { stdio: 'inherit' });
+execSync('npm publish --tag ${tag}', { stdio: 'inherit' });
 
 packageJson.version = originalVersion;
 writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
