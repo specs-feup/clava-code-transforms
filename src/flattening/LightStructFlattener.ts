3e6b6810b0cb03feb17e87c41f37662fe0d2b87c
tiagolascasas <tiagolascasas@outlook.com>
2025-10-17 03:24:19 +0100

    Flattening structs in function calls

--- src/flattening/LightStructFlattener.ts
+++ src/flattening/LightStructFlattener.ts
@@ -1,6 +1,6 @@
 import Query from "@specs-feup/lara/api/weaver/Query.js";
 import ClavaJoinPoints from "@specs-feup/clava/api/clava/ClavaJoinPoints.js"
-import { ArrayType, Expression, Field, FunctionJp, IncompleteArrayType, MemberAccess, Param, Statement, Type, VariableArrayType, Varref } from "@specs-feup/clava/api/Joinpoints.js"
+import { ArrayType, Call, Expression, Field, FunctionJp, IncompleteArrayType, MemberAccess, Param, Statement, Type, VariableArrayType, Varref } from "@specs-feup/clava/api/Joinpoints.js"
 import Clava from "@specs-feup/clava/api/clava/Clava.js";
 import { StructFlatteningAlgorithm } from "./StructFlatteningAlgorithm.js";
 
@@ -116,12 +116,66 @@ export class LightStructFlattener extends StructFlatteningAlgorithm {
         return 0;
     }
 
-    flattenAssignments(fun: FunctionJp, fields: Field[], name: string) {
+    private flattenAssignments(fun: FunctionJp, fields: Field[], name: string) {
         return 0;
     }
 
-    flattenCalls(fun: FunctionJp, fields: Field[], name: string) {
-        return 0;
+    private flattenCalls(fun: FunctionJp, fields: Field[], name: string) {
+        let changes = 0;
+
+        for (const call of Query.searchFrom(fun, Call)) {
+            changes += this.flattenCall(call, fields, name);
+        }
+        return changes;
+    }
+
+    private flattenCall(call: Call, fields: Field[], name: string): number {
+        let changes = 0;
+
+        const idxToFlatten: number[] = [];
+        for (let i = 0; i < call.args.length; i++) {
+            const arg = call.args[i];
+            // we assume that any foo->bar or foo.bar is already flattened
+            // so there are only struct refs left to flatten at this point
+            if (arg.type.code.includes(name)) {
+                idxToFlatten.push(i);
+            }
+        }
+        if (idxToFlatten.length === 0) {
+            return changes;
+        }
+
+        const newArgList: Expression[] = [];
+        for (let i = 0; i < call.args.length; i++) {
+            const arg = call.args[i];
+            if (!idxToFlatten.includes(i)) {
+                newArgList.push(arg);
+                continue;
+            }
+            else {
+                const newArgs = this.flattenCallArg(arg, fields);
+                newArgList.push(...newArgs);
+                changes += 1;
+            }
+        };
+        const currNArgs = call.args.length;
+        for (let i = 0; i < currNArgs; i++) {
+            call.setArg(i, newArgList[i]);
+        }
+        for (let i = currNArgs; i < newArgList.length; i++) {
+            call.addArg(newArgList[i].code, newArgList[i].type);
+        }
+        return changes;
+    }
+
+    private flattenCallArg(arg: Expression, fields: Field[]): Expression[] {
+        const newArgs: Expression[] = [];
+        fields.forEach((field) => {
+            const newArgName = `${arg.code}_${field.name}`;
+            const newArg = ClavaJoinPoints.exprLiteral(newArgName);
+            newArgs.push(newArg);
+        });
+        return newArgs;
     }
 
     // -----------------------------------------------------------------------
